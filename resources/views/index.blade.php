bb60f9fdac040cdb1332cf63a325a90ba0d716f3
adrianpaskalis <adrian.paskalis@si.ukdw.ac.id>
2020-03-08 21:50:53 +0700

    masukin bootstrap tp blm bisa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,53 +2,63 @@
 <html>
 <head>
 	<title>Tutorial Membuat Pencarian Pada Laravel - www.malasngoding.com</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}">
 </head>
 <body>
 
-	<style type="text/css">
-		.pagination li{
-			float: left;
-			list-style-type: none;
-			margin:5px;
-		}
-	</style>
+	<div class="container">
+		<div class="card">
+			<div class="card-body">
+				
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Pegawai</h3>
+				<h2 class="text-center"><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
 
+				<h3>Data Pegawai</h3>
 
-	<p>Cari Data Pegawai :</p>
-	<form action="/pegawai/cari" method="GET">
-		<input type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old('cari') }}">
-		<input type="submit" value="CARI">
-	</form>
-		
-	<br/>
+				<p>Cari Data Pegawai :</p>
 
-	<table border="1">
-		<tr>
-			<th>Nama</th>
-			<th>Jabatan</th>
-			<th>Umur</th>
-			<th>Alamat</th>
-		</tr>
-		@foreach($pegawai as $p)
-		<tr>
-			<td>{{ $p->pegawai_nama }}</td>
-			<td>{{ $p->pegawai_jabatan }}</td>
-			<td>{{ $p->pegawai_umur }}</td>
-			<td>{{ $p->pegawai_alamat }}</td>
-		</tr>
-		@endforeach
-	</table>
+				<div class="form-group">
+					
+				</div>
+				<form action="/pegawai/cari" method="GET" class="form-inline">
+					<input class="form-control" type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old('cari') }}">
+					<input class="btn btn-primary ml-3" type="submit" value="CARI">
+				</form>
 
-	<br/>
-	Halaman : {{ $pegawai->currentPage() }} <br/>
-	Jumlah Data : {{ $pegawai->total() }} <br/>
-	Data Per Halaman : {{ $pegawai->perPage() }} <br/>
+				<br/>
 
+				<table class="table table-bordered">
+					<tr>
+						<th>Nama</th>
+						<th>Jabatan</th>
+						<th>Umur</th>
+						<th>Alamat</th>
+						<th>Opsi</th>
+					</tr>
+					@foreach($pegawai as $p)
+					<tr>
+						<td>{{ $p->pegawai_nama }}</td>
+						<td>{{ $p->pegawai_jabatan }}</td>
+						<td>{{ $p->pegawai_umur }}</td>
+						<td>{{ $p->pegawai_alamat }}</td>
+						<td>
+							<a class="btn btn-warning btn-sm" href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+							<a class="btn btn-danger btn-sm" href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
+						</td>
+					</tr>
+					@endforeach
+				</table>
 
-	{{ $pegawai->links() }}
+				<br/>
+				Halaman : {{ $pegawai->currentPage() }} <br/>
+				Jumlah Data : {{ $pegawai->total() }} <br/>
+				Data Per Halaman : {{ $pegawai->perPage() }} <br/>
+				<br/>
+
+				{{ $pegawai->links() }}
+			</div>
+		</div>
+	</div>
 
 
 </body>
